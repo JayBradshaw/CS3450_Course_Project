8b92150b12ac755d56d1fa4b4910d6c4067f6c5f
Justin Bradshaw <justin.bradshaw@aggiemail.usu.edu>
2017-02-06 14:27:11 -0700

    Initial commit of course project

---
+++ Course_Project/src/CS3450/course_project/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package CS3450.course_project;
+
+/**
+ * @author justin
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Grocery Store");
+	}
+
+}
