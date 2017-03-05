322953ae6bc2d08ab3a48724f203d9aa8322cf97
Melanie Pena <penamelanie3@gmail.com>
2017-03-04 18:54:00 -0700

    added inventory screen class

--- Course_Project/src/CS3450/course_project/userInterface/MainScreen.java
+++ Course_Project/src/CS3450/course_project/userInterface/MainScreen.java
@@ -134,6 +134,7 @@ public class MainScreen {
 						//frame.dispose(); //close the frame
 						//create a new frame here for the manage inventory screen
 						System.out.println("Creating new page for Managing Inventory");
+						InventoryScreen i = new InventoryScreen();
 					}
 					
 				});
